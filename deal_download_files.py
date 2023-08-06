5c0982eb6c47dee3437374a30fd84e68c29000fe
zhengqwe <103168200+zhengqwe@users.noreply.github.com>
2023-08-06 18:40:16 +0800

    Update deal_download_files.py

--- deal_download_files.py
+++ deal_download_files.py
@@ -5,14 +5,12 @@ os.system('rm -rf result')
 os.mkdir('result')
 
 for dirpath, dirnames, filenames in os.walk("downloads"):
-    os.chdir(dirpath)
     for filename in filenames:
         # 压缩文件
         cmd = '7z a -t7z -m0=Copy -sccUTF-8 -v1024m -sdel "{afile}" "{sourcefile}"'.format(
-            afile=path.join('../result', filename + '.7z'),
-            sourcefile=filename
+            afile=path.join('./result', filename + '.7z'),
+            sourcefile=path.join(dirpath, filename)
         )
         print("[deal] %s with %s" % (path.join(dirpath, filename), cmd))
         os.system(cmd)
         # 删除原始文件
-    os.chdir('..')
