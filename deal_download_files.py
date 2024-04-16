30be6220fd9c06411d552779ff09866283eb7f4c
zhengqwe <103168200+zhengqwe@users.noreply.github.com>
2024-04-16 21:23:10 +0800

    Update deal_download_files.py

--- deal_download_files.py
+++ deal_download_files.py
@@ -7,9 +7,16 @@ os.mkdir('result')
 for dirpath, dirnames, filenames in os.walk("downloads"):
     for filename in filenames:
         # 压缩文件
-        cmd = '7z a -t7z -m0=Copy -sccUTF-8 -v1536m -sdel "{afile}" "{sourcefile}"'.format(
-            afile=path.join('./result', filename + '.7z'),
-            sourcefile=path.join(dirpath, filename)
-        )
+        fsize=path.getsize(filename)
+        if fsize>1610612736:
+            cmd = '7z a -t7z -m0=Copy -sccUTF-8 -v1536m -sdel "{afile}" "{sourcefile}"'.format(
+                afile=path.join('./result', filename + '.7z'),
+                sourcefile=path.join(dirpath, filename)
+            )
+        else:
+        	cmd = '7z a -t7z -m0=Copy -sccUTF-8 -sdel "{afile}" "{sourcefile}"'.format(
+                afile=path.join('./result', filename + '.7z'),
+                sourcefile=path.join(dirpath, filename)
+            )
         print("[deal] %s with %s" % (path.join(dirpath, filename), cmd))
         os.system(cmd)
